f7fbab4ffb3c13fefcae318dd23061caed6e06af
KittyLiu <kab49abc@gmail.com>
2016-12-16 10:49:52 +0800

    first commit

---
+++ kittyweatherbot/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KittyweatherbotConfig(AppConfig):
+    name = 'kittyweatherbot'
---
+++ kittyweatherbot/urls.py
@@ -0,0 +1,10 @@
+# kittyweatherbot/urls.py
+
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url('^callback/', views.callback),
+]
