850e8f4123706eccec29a7583d641887d5363605
KittyLiu <kab49abc@gmail.com>
2016-12-27 23:37:47 +0800

    able to respond with Tainan's weather

--- kittyweatherbot/views.py
+++ kittyweatherbot/views.py
@@ -9,6 +9,9 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+import urllib.parse
+import urllib.request
+
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
@@ -30,9 +33,18 @@ def callback(request):
             if isinstance(event, MessageEvent):
                 if isinstance(event.message, TextMessage):
                     if '天氣' in event.message.text:
+                        req = urllib.request.Request('http://opendata.cwb.gov.tw/opendataapi?dataid=F-C0032-001&authorizationkey=CWB-8C4DB566-AD62-4E69-91D8-6E7323B5F20A')
+                        resp = urllib.request.urlopen(req)
+                        content = resp.read()
+                        content = content.decode().split('臺南市')
+                        content = content[1].split('<time>')
+                        content = content[1].split('<parameterName>')
+                        endpoint = content[1].find('</parameterName>')
+                        result = content[1][:endpoint]
+                        print(result)
                         line_bot_api.reply_message(
                             event.reply_token,
-                            TextSendMessage(text='你是想問天氣嗎?')
+                            TextSendMessage(text=result)
                         )
                     else:
                         line_bot_api.reply_message(
